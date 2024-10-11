fcd4b42d31051fb06b87e82a380a88263ffcf8c4
grrowerr <grouw.uzkiy@yandex.ru>
2024-10-12 01:24:08 +0300

    fix: -add sout for UserDaoJDBCImpl.saveUser; -indentation in pom.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -46,6 +46,7 @@ public class UserDaoJDBCImpl implements UserDao {
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
             preparedStatement.executeUpdate();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
         } catch (Exception e) {
             e.printStackTrace();
         }
